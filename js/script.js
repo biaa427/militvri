37a93e16d5961edaedb3e748bd06c7cd858bfc29
biaa427 <cbioana4@gmail.com>
2023-11-01 14:41:30 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* ------------------------------- photo slider buttons -------------------------- */
+
 var imgList = document.getElementById("img-list");
 var caroBtns = document.getElementsByClassName("caro-btn");
 
@@ -15,4 +17,118 @@ function slideImg(x) {
     caroBtns[x].style.backgroundColor = "#fff";
     position = x * positionUit;
     imgList.style.left = position + "%";
+};
+
+/* ------------------------------- show navigation --------------------------------- */
+
+$(document).ready(function() {
+    $('#navigationContainer').load('nav.html .navigation');
+});
+
+/* ------------------------------- shopping cart ------------------------------------ */
+
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.cart-list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'PRODUCT NAME 1',
+        image: 'product-1.jpeg',
+        price: 29
+    },
+    {
+        id: 2,
+        name: 'PRODUCT NAME 2',
+        image: 'product-2.jpeg',
+        price: 29
+    },
+    {
+        id: 3,
+        name: 'PRODUCT NAME 3',
+        image: 'product-3.jpeg',
+        price: 29
+    },
+    {
+        id: 4,
+        name: 'PRODUCT NAME 4',
+        image: 'product-4.jpeg',
+        price: 29
+    },
+];
+let listCards = [];
+function initApp(){
+    products.forEach((value, key)=>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item-cart');
+        newDiv.innerHTML = `
+        <img src="images/${value.image}"/>
+        <div class="cartItem-title">${value.name}</div>
+        <div class="cartItem-price">${value.price.toLocaleString()}</div>
+        <button onclick="addToCart(${key})">Add To Cart</button>
+        `;
+        list.appendChild(newDiv);
+    })
+}
+    
+initApp();
+
+function addToCart(key) {
+    if (listCards[key] == null) {
+        listCards[key] = products[key];
+        listCards[key].quantity = 1
+    }
+    reloadCart();
+}
+
+function reloadCart() {
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key) => {
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+
+        if (value != null) {
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="images/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>${value.quantity}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>
+
+            `
+            listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText =  count;
+}
+
+function changeQuantity(key, quantity) {
+    if (quantity == 0) {
+        delete listCards[key];
+    } else {
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCart();
 }
