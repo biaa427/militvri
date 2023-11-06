c6c26f70e486d4e4a7134e17dd693317e1f543bd
biaa427 <cbioana4@gmail.com>
2023-11-06 09:28:48 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+/* hamburger menu */
+
+$(document).ready(function() {
+    $(".toggle-box").click(function(e) {
+        e.stopPropagation(); // Prevents the click event from propagating to parent elements
+        $(".menu").stop().slideToggle();
+    });
+
+    // Check window width and handle menu item clicks
+    $(".menu li a").click(function() {
+        if ($(window).width() < 1100) {
+            $(".menu").slideUp(); // Closes the menu when a menu item is clicked on smaller screens
+        }
+    });
+
+});
+
 /* photo slider buttons */
 
 var imgList = document.getElementById("img-list");
@@ -33,37 +50,39 @@ let products = [
         name: 'PRODUCT NAME 1',
         image: 'product-1.jpeg',
         price: 29,
-        serial_no: 135
+        serial_no: "135"
     },
     {
         id: 2,
         name: 'PRODUCT NAME 2',
         image: 'product-2.jpeg',
         price: 29,
-        serial_no: 222
+        serial_no: "222"
     },
     {
         id: 3,
         name: 'PRODUCT NAME 3',
         image: 'product-3.jpeg',
         price: 29,
-        serial_no: 210
+        serial_no: "210"
     },
     {
         id: 4,
         name: 'PRODUCT NAME 4',
         image: 'product-4.jpeg',
         price: 29,
-        serial_no: 658
+        serial_no: "658"
     },
 ];
+
+let cart = [];
 
 function loadProducts() {
     products.forEach((value, key) => {
         let newDiv = document.createElement('div');
         newDiv.classList.add('product');
         newDiv.innerHTML = `
-        <div class="product-box" id="${value.id}">
+        <div class="product-box" id="${value.serial_no}">
         <img src="images//${value.image}" alt="" class="product-image" style="cursor: pointer" onclick="openProduct(this)">
             <div class="product-info">
                 <h4 class="product-title">${value.name}</h4>
@@ -145,7 +164,24 @@ function ready() {
     }
 }
 
+function saveCartToLocalStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
 function removeCartItem(buttonClicked) {
+    // Find the index of the cart box in the cart content
+    var cartContent = document.querySelector(".cart-content");
+    const cartItem = buttonClicked.parentElement;
+    const index = Array.from(cartContent.children).indexOf(cartItem);
+
+    // Remove the item from the cart array
+    cart.splice(index, 1);
+
+    // Remove the cart box from the cart content
+    cartItem.remove();
+
+    saveCartToLocalStorage();
+
     buttonClicked.parentElement.remove();
     updateTotal();
     cartCountItems();
@@ -158,6 +194,18 @@ function removeCartItem(buttonClicked) {
     }
     updateTotal();
     cartCountItems();
+
+    var detailBox = q.parentElement;
+    var cartSerialNo = detailBox.querySelector('.cart-serial-no');
+    var serialNumber = cartSerialNo.textContent;
+
+    const cartProductIndex = cart.findIndex(product => product.serial_no === parseInt(serialNumber, 10));
+    if (cartProductIndex !== -1) {
+        cart[cartProductIndex].quantity = parseInt(q.value, 10);
+    }
+
+    // Save the updated cart array in localStorage or perform other necessary operations
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 function addCartClicked(buttonAdd) {
@@ -190,35 +238,36 @@ function addCartClicked(buttonAdd) {
 }
 
 function addProductToCart(title, price, productImg, serial_no) {
-    var cartContent = document.querySelector(".cart-content");
+
+    var cartContent = document.querySelector('.cart-content');
     var cartBox = document.createElement("div");
     cartBox.classList.add("cart-box");
-
+    
     var img = document.createElement("img");
     img.src = productImg;
     img.alt = title;
     img.classList.add("cart-img");
     cartBox.appendChild(img);
-
+    
     var detailBox = document.createElement("div");
     detailBox.classList.add("detail-box");
-
+    
     var cartProductTitle = document.createElement("div");
     cartProductTitle.classList.add("cart-product-title");
     cartProductTitle.textContent = title;
     detailBox.appendChild(cartProductTitle);
-
+    
     var cartPrice = document.createElement("div");
     cartPrice.classList.add("cart-price");
     cartPrice.textContent = price;
     detailBox.appendChild(cartPrice);
-
+    
     var cartSerialNo = document.createElement("div");
     cartSerialNo.classList.add("cart-serial-no");
     cartSerialNo.style.display = "none";
     cartSerialNo.textContent = serial_no;
     detailBox.appendChild(cartSerialNo);
-
+    
     var quantityInput = document.createElement("input");
     quantityInput.type = "number";
     quantityInput.value = 1;
@@ -229,7 +278,36 @@ function addProductToCart(title, price, productImg, serial_no) {
         quantityChanged(this);
     });
     detailBox.appendChild(quantityInput);
-
+    
+    var sizeOptionXS = document.createElement("option");
+    sizeOptionXS.value = "XS";
+    sizeOptionXS.innerText = "XS";
+    
+    var sizeOptionS = document.createElement("option");
+    sizeOptionS.value = "S";
+    sizeOptionS.innerText = "S";
+    
+    var sizeOptionM = document.createElement("option");
+    sizeOptionM.value = "M";
+    sizeOptionM.innerText = "M";
+    
+    var sizeOptionL = document.createElement("option");
+    sizeOptionL.value = "L";
+    sizeOptionL.innerText = "L";
+    
+    var sizeOptionXL = document.createElement("option");
+    sizeOptionXL.value = "XL";
+    sizeOptionXL.innerText = "XL";
+    
+    var sizeInput = document.createElement("select");
+    sizeInput.classList.add("cart-size");
+    sizeInput.appendChild(sizeOptionXS);
+    sizeInput.appendChild(sizeOptionS);
+    sizeInput.appendChild(sizeOptionM);
+    sizeInput.appendChild(sizeOptionL);
+    sizeInput.appendChild(sizeOptionXL);
+    detailBox.appendChild(sizeInput);
+    
     cartBox.appendChild(detailBox);
 
     var removeButton = document.createElement("button");
@@ -245,8 +323,22 @@ function addProductToCart(title, price, productImg, serial_no) {
     removeButton.appendChild(removeIcon);
     cartBox.appendChild(removeButton);
 
-    cartContent.appendChild(cartBox);
+    cartContent.appendChild(cartBox); // Append the product details to the cart content
+
+    const product = {
+        title: title,
+        price: price,
+        serial_no: serial_no,
+        image: productImg,
+        quantity: 1, // Initialize quantity to 1
+    };
+    cart.push(product);
+
+    saveCartToLocalStorage(); // Save the updated cart to localStorage
+
+    displayCartItems(); // Refresh the cart display
 }
+
 
  function updateTotal() {
     var cartBoxes = document.getElementsByClassName("cart-box");
@@ -279,7 +371,7 @@ function openProduct(img) {
 
     var id = productBox.id;
 
-    window.location.href = "product.html?id=" + id;
+    window.location.href = "product.html?sn=" + id;
 }
 
 function checkCartItems() {
@@ -288,14 +380,17 @@ function checkCartItems() {
     var sharedContainer = document.querySelector(".shared-container");
     var navigation = sharedContainer.querySelector(".navigation");
     var logoCart = navigation.querySelector(".logo-cart");
+    var cartBtn = navigation.querySelector(".cart-btn");
 
     if (cartContent !== null) {
         if (cartContent.children.length > 0) {
             logoCartImg.src = "images/logo-cart-full-black.png";
             logoCart.style.setProperty("content", "url(../images/logo-cart-full-black.png)")
+            cartBtn.style.padding = "0.2rem 0.93rem";
         } else {
             logoCartImg.src = "images/logo-cart-empty-black.png";
             logoCart.style.setProperty("content", "url(../images/logo-cart-empty-black.png)")
+            cartBtn.style.padding = "0.5rem 0.93rem";
         }
     }
 }
@@ -314,11 +409,115 @@ function cartCountItems() {
         var itemQuantity = parseInt(cartBoxes[i].querySelector(".cart-quantity").value, 10);
         count = count + itemQuantity;
     }
-
-    // Get the count of cart boxes
-    var numberOfItems = cartBoxes.length;
-
     // Update the cart count element with the number of items
     cartCount.textContent = count;
-    console.log(count);
 }
+
+// Display cart items based on the serial numbers
+function displayCartItems() {
+    const cartContent = document.querySelector(".cart-content");
+
+    cart.forEach(cartProduct => {
+        const foundProduct = products.find(product => product.serial_no === cartProduct.serial_no);
+
+        if (foundProduct) {
+            if (cartContent !== null) {
+                var cartBox = document.createElement("div");
+                cartBox.classList.add("cart-box");
+    
+                var img = document.createElement("img");
+                img.src = "images/" + foundProduct.image;
+                img.alt = foundProduct.name;
+                img.classList.add("cart-img");
+                cartBox.appendChild(img);
+    
+                var detailBox = document.createElement("div");
+                detailBox.classList.add("detail-box");
+    
+                var cartProductTitle = document.createElement("div");
+                cartProductTitle.classList.add("cart-product-title");
+                cartProductTitle.textContent = foundProduct.name;
+                detailBox.appendChild(cartProductTitle);
+    
+                var cartPrice = document.createElement("div");
+                cartPrice.classList.add("cart-price");
+                cartPrice.textContent = foundProduct.price;
+                detailBox.appendChild(cartPrice);
+    
+                var cartSerialNo = document.createElement("div");
+                cartSerialNo.classList.add("cart-serial-no");
+                cartSerialNo.style.display = "none";
+                cartSerialNo.textContent = foundProduct.serial_no;
+                detailBox.appendChild(cartSerialNo);
+    
+                var quantityInput = document.createElement("input");
+                quantityInput.type = "number";
+                quantityInput.value = cartProduct.quantity || 1;
+                quantityInput.classList.add("cart-quantity");
+                quantityInput.setAttribute("title", "quantity");
+                quantityInput.placeholder = "";
+                quantityInput.addEventListener("change", function() {
+                    quantityChanged(this);
+                });
+                detailBox.appendChild(quantityInput);
+    
+                var sizeOptionXS = document.createElement("option");
+                sizeOptionXS.value = "XS";
+                sizeOptionXS.innerText = "XS";
+    
+                var sizeOptionS = document.createElement("option");
+                sizeOptionS.value = "S";
+                sizeOptionS.innerText = "S";
+    
+                var sizeOptionM = document.createElement("option");
+                sizeOptionM.value = "M";
+                sizeOptionM.innerText = "M";
+    
+                var sizeOptionL = document.createElement("option");
+                sizeOptionL.value = "L";
+                sizeOptionL.innerText = "L";
+    
+                var sizeOptionXL = document.createElement("option");
+                sizeOptionXL.value = "XL";
+                sizeOptionXL.innerText = "XL";
+    
+                var sizeInput = document.createElement("select");
+                sizeInput.classList.add("cart-size");
+                sizeInput.appendChild(sizeOptionXS);
+                sizeInput.appendChild(sizeOptionS);
+                sizeInput.appendChild(sizeOptionM);
+                sizeInput.appendChild(sizeOptionL);
+                sizeInput.appendChild(sizeOptionXL);
+                detailBox.appendChild(sizeInput);
+    
+                cartBox.appendChild(detailBox);
+    
+                var removeButton = document.createElement("button");
+                removeButton.type = "button";
+                removeButton.classList.add("cart-remove");
+                removeButton.setAttribute("aria-label", "Remove item from cart");
+                removeButton.addEventListener("click", function() {
+                    removeCartItem(this);
+                });
+    
+                var removeIcon = document.createElement("i");
+                removeIcon.classList.add("bx", "bxs-trash");
+                removeButton.appendChild(removeIcon);
+                cartBox.appendChild(removeButton);
+    
+                cartContent.appendChild(cartBox); // Append the product details to the cart content
+            }
+        }
+    });
+}
+
+window.onload = function() {
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart) {
+        cart = JSON.parse(storedCart);
+        displayCartItems();
+        //checkCartItems();
+        cartCountItems();
+    }
+}
+8
